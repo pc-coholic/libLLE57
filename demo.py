f57762f7e2ffedfac3b1b47116cbb670d47f747e
Martin Gross <martin@pc-coholic.de>
2013-05-31 13:18:23 +0200

    New demo, allowing the iteration through all 3x11 (2x11, 1x10 respectivly) panels of HFG

--- demo.py
+++ demo.py
@@ -1,18 +1,26 @@
 from libLLE57 import LLE57
 import time
 
-treppe = LLE57()
+treppen = []
+for i in range(0, 3):
+	treppen.append(LLE57(serialport = '/dev/ttyUSB3', slave = i + 1))
+	treppen[i].cmd_set_conserve_font()
+	treppen[i].cmd_set_text(text = "", output = '0x04')
+	treppen[i].cmd_set_text(text = "", output = '0x01')
+	treppen[i].send(True)
+	treppen[i].flush()
 
-treppe.cmd_set_time()
-print treppe.send(True)
-time.sleep(5)
+time.sleep(1)
 
-treppe.flush()
-treppe.cmd_set_conserve_font()
-treppe.cmd_set_conserve_text()
-print treppe.send(True)
-time.sleep(5)
+for i in range(0, 3):
+	treppen[i].cmd_set_text(text = "", output = '0x00')
+	treppen[i].send(True)
+	treppen[i].flush()
 
-treppe.flush()
-treppe.cmd_set_text("Hallo Welt")
-print treppe.send(True)
+for i in range(0, 11):
+	for j in range (0, len(treppen)):
+		treppen[j].cmd_set_text(text = "Slave " + str(j + 1) + "/Line " + str(i + 1),
+					starty = i * 7, output = '0x01', attrib = '0x10')
+		treppen[j].send(True)
+		treppen[j].flush()
+
